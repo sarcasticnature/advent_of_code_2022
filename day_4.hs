73bbafe3df8fcad160d1b246eeca6af04d0eda52
Jake Keller <jakeller3630@gmail.com>
2022-12-31 19:55:13 -0600

    Refactor day 4

--- day_4.hs
+++ day_4.hs
@@ -5,7 +5,7 @@ main = do
     filename_list <- getArgs
     let filename = head filename_list
     contents <- readFile filename
-    print $ numTrue $ map isFullyOverlapping $ lines contents
+    print $ numTrue $ map (isFullyOverlapping . parseAssignment) $ lines contents
 
 -- could use the standard library isInfixOf instead (it's probably better)
 --isSublist :: Eq a => [a] -> [a] -> Bool
@@ -13,16 +13,25 @@ main = do
 --isSublist needle stack =
 --    (needle == take (length needle) stack) || isSublist needle (tail stack)
 
-isFullyOverlapping :: String -> Bool
-isFullyOverlapping str = 
-    (start1 <= start2) && (end1 >= end2) || (start2 <= start1) && (end2 >= end1)
-    where (first, _:second) = span (/= ',') str
-          getStart = read . takeWhile (/= '-') :: String -> Int
-          getEnd = read . tail . dropWhile (/= '-') :: String -> Int
-          start1 = getStart first
-          end1 = getEnd first
-          start2 = getStart second
-          end2 = getEnd second
+data Assignment = Assignment { start1 :: Int
+                             , end1 :: Int
+                             , start2 :: Int
+                             , end2 :: Int }
+
+parseAssignment :: String -> Assignment
+parseAssignment str =
+    let getStart = read . takeWhile (/= '-') :: String -> Int
+        getEnd = read . tail . dropWhile (/= '-') :: String -> Int
+        (first, _:second) = span (/= ',') str
+        start1 = getStart first
+        end1 = getEnd first
+        start2 = getStart second
+        end2 = getEnd second
+    in  Assignment start1 end1 start2 end2
+
+isFullyOverlapping :: Assignment -> Bool
+isFullyOverlapping (Assignment s1 e1 s2 e2) =
+    (s1 <= s2) && (e1 >= e2) || (s2 <= s1) && (e2 >= e1)
 
 numTrue :: [Bool] -> Int
 numTrue = length . filter id
