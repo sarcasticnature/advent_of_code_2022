806c79d97336d8e7e1cbb78cd224874ed9e56702
Jake Keller <jakeller3630@gmail.com>
2022-12-31 18:10:20 -0600

    Finish day 3 part 2

--- day_3.hs
+++ day_3.hs
@@ -7,7 +7,7 @@ main = do
     filename_list <- getArgs
     let filename = head filename_list
     contents <- readFile filename
-    putStr $ show $ scoreDuplicates $ parseForDuplicates contents
+    print $ scoreBadges $ map getGroupBadge $ groupPacks $ splitBackpack contents
 
 splitBackpack :: String -> [String]
 splitBackpack [] = []
@@ -15,22 +15,35 @@ splitBackpack str = let (x, xs:xs') = span (/= '\n') str
                     in  x : splitBackpack xs'     -- need to get rid of the leading newline
 
 splitPouch :: String -> (String,String)
-splitPouch x = splitAt ((length x) `div` 2) x
+splitPouch x = splitAt (length x `div` 2) x
 
-getPouchDuplicates :: (String,String) -> String
-getPouchDuplicates (a,b) = filter (\x -> elem x b) a
+getPairDuplicates :: (String,String) -> String
+getPairDuplicates (a,b) = filter (`elem` b) a
 
 parseForDuplicates :: String -> [String]
 parseForDuplicates =
-    map nub . map getPouchDuplicates . map splitPouch . splitBackpack
+    map (nub . getPairDuplicates . splitPouch) . splitBackpack
 
 scoreItem :: Char -> Int
 scoreItem c
-    | isLower c = (ord c) - 96
-    | isUpper c = (ord c) - 38
+    | isLower c = ord c - 96
+    | isUpper c = ord c - 38
     | otherwise = error "bad character in input"
 
 scoreDuplicates :: [String] -> Int
 scoreDuplicates dupList =
-    let foldFunc acc str = acc + (sum $ map scoreItem str)
+    let foldFunc acc str = acc + sum (map scoreItem str)
     in  foldl foldFunc 0 dupList
+
+groupPacks :: [String] -> [(String,String,String)]
+groupPacks [] = []
+groupPacks (x:y:z:ws) = (x,y,z) : groupPacks ws
+
+getGroupBadge :: (String,String,String) -> Char
+getGroupBadge (x,y,z) =
+    let dups1 = getPairDuplicates (x,y)
+        dups2 = filter (`elem` z) dups1
+    in  head dups2
+
+scoreBadges :: [Char] -> Int
+scoreBadges = sum . map scoreItem
