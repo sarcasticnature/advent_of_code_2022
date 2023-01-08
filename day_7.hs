6aa2c92d5739d1ee486e21a5fe77f2deed44d01d
Jake Keller <jakeller3630@gmail.com>
2023-01-08 17:24:08 -0600

    Finish day 7 part 1

--- day_7.hs
+++ day_7.hs
@@ -6,22 +6,22 @@ main = do
     filename_list <- getArgs
     let filename = head filename_list
     contents <- readFile filename
-    print $ parseInput $ lines contents
+    --print $ fmap debugZipper $ parseInput $ lines contents
+    print $ fmap (sumSmallDirs 100000) $ parseInput $ lines contents
 
 -- Data
-type FSSize = Int
-type FSName = String
-data FSData = File FSName FSSize
-            | Dir FSName [FSData]
-            deriving (Show,Eq)
+data FileSystem a = File String a
+                  | Dir String [FileSystem a]
+                  deriving (Show,Eq)
+type FileSystemInt = FileSystem Int
 
-data FSParent = Parent { name :: FSName
-                       , files :: [FSData]
+data FSParent = Parent { name :: String
+                       , files :: [FileSystemInt]
                        } deriving (Show)
 type FSParents = [FSParent]
-type FSZipper = (FSParents, FSData)
+type FSZipper = (FSParents, FileSystemInt)
 
-data CDCommand = Descend FSName
+data CDCommand = Descend String
                | Ascend
                | GoToRoot
                deriving (Show)
@@ -44,8 +44,8 @@ parseCommandLine line = case words line of
             | dir == ".." = CD Ascend
             | otherwise = CD (Descend dir)
 
--- TODO: make this return Maybe FSData to handle bad input
-parseFSDataLine :: String -> FSData
+-- TODO: make this return Maybe FileSystemInt to handle bad input
+parseFSDataLine :: String -> FileSystemInt
 parseFSDataLine line =
     if dir
     then Dir name []
@@ -55,19 +55,6 @@ parseFSDataLine line =
         dir = head xs == "dir"
         name = last xs
         size = read $ head xs
-
---testParseCmd :: [String] -> [FSCommand]
---testParseCmd lines =
---    let cmdLines = filter isFSCommand lines
---        foldFn acc cmdLine = case parseCommandLine cmdLine of Just cmd -> cmd:acc
---                                                              Nothing  -> acc
---    in  foldl' foldFn [] cmdLines
---
---testParseFSDataLine :: [String] -> [FSData]
---testParseFSDataLine lines =
---    let dataLines = filter (not . isFSCommand) lines
---        foldFn acc line = parseFSDataLine line : acc
---    in  foldl' foldFn [] dataLines
 
 ascend :: FSZipper -> Maybe FSZipper
 ascend ([], _) = Nothing
@@ -90,18 +77,18 @@ cd (ps, Dir n fs) (Descend subdir)
 
 cd _ _ = Nothing
 
--- TODO: use updated parseFSDataLine (which would return Maybe FSData
+-- TODO: use updated parseFSDataLine (which would return Maybe FileSystemInt
 parseLS :: FSZipper -> [String] -> Maybe (FSZipper, [String])
 parseLS (_, File _ _) _ = Nothing
-parseLS (parents, Dir name ys) xs =
+parseLS (parents, Dir name fs) xs =
     let (ds, xs') = break isFSCommand xs
         foldFn acc line = parseFSDataLine line : acc
         dataList = foldl' foldFn [] ds
-        dir = Dir name (dataList ++ ys)
+        dir = Dir name (dataList ++ fs)
     in  Just ((parents, dir), xs')
 
 munchInputLines :: FSZipper -> [String] -> Maybe (FSZipper, [String])
-munchInputLines zipper [] = Just (zipper, [])   -- careful: this could lead to an infinite loop
+munchInputLines zipper [] = Just (zipper, [])
 munchInputLines zipper (x:xs) = case parseCommandLine x of
     Nothing       -> Nothing
     Just LS       -> parseLS zipper xs
@@ -118,3 +105,37 @@ parseInput xs =
     in  parse ([], Dir "/" []) xs
 
 -- algorithm
+
+dirSize :: (Num a) => FileSystem a -> a
+dirSize (File _ _) = error "dirSize called on a File instead of a Dir"
+dirSize dir = getSize dir
+    where getSize (File _ sz) = sz
+          getSize (Dir n fs) = sum $ map getSize fs
+
+getDirSizes :: (Num a) => FileSystem a -> [a]
+getDirSizes (File _ _) = []
+getDirSizes d@(Dir n fs) =
+    let rootSize = dirSize d
+        getSize [] = []
+        getSize ((File _ _):fs') = getSize fs'
+        getSize (d'@(Dir _ fs''):fs') = dirSize d' : getSize fs'' ++ getSize fs'
+    in  rootSize : getSize fs
+
+sumSmallDirs :: Int -> FSZipper -> Int
+sumSmallDirs max z = case cd z GoToRoot of
+    Nothing           -> 0
+    Just (_, rootDir) -> sum $ filter (< max) $ getDirSizes rootDir
+
+-- debugging
+debugZipper z = case cd z GoToRoot of
+    Nothing           -> []
+    Just (_, rootDir) -> getDirInfo rootDir
+
+getDirInfo :: (Num a) => FileSystem a -> [(String, a)]
+getDirInfo (File _ _) = []
+getDirInfo d@(Dir n fs) =
+    let rootInfo = (n, dirSize d)
+        getInfo [] = []
+        getInfo ((File _ _):fs') = getInfo fs'
+        getInfo (d'@(Dir n' fs''):fs') = (n', dirSize d') : getInfo fs'' ++ getInfo fs'
+    in  rootInfo : getInfo fs
